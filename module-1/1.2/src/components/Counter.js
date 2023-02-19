3db7008caddf4ed7563c15ea9534722d74e63f2a
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-02-19 15:16:47 +0600

    incremet and decrement handler functional

--- module-1/1.2/src/components/Counter.js
+++ module-1/1.2/src/components/Counter.js
@@ -6,6 +6,10 @@ const Counter = () => {
     const increment = () => {
         setCount((prevCount) => prevCount + 1);
     }
+
+    const decrement = () => {
+        setCount((prevCount) => prevCount - 1);
+    }
      
     return (
         <div>
@@ -16,11 +20,13 @@ const Counter = () => {
                 <div class="flex space-x-3">
                     <button
                         class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+                        onClick={increment}
                     >
                         Increment
                     </button>
                     <button
                         class="bg-red-400 text-white px-3 py-2 rounded shadow"
+                        onClick={decrement}
                     >
                         Decrement
                     </button>
